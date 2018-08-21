a198c32aff52a27eabbd432ca7a919c72cdbdd07
Emmanuel <31184578+emthecoder@users.noreply.github.com>
2018-08-21 17:37:59 +0100

    variables_and_expressions done.

--- 1_variables_n_expressions/exercise_1.py
+++ 1_variables_n_expressions/exercise_1.py
@@ -1,1 +1,2 @@
-#place your code here
+wd0, wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8, wd9 ="All", "work", "and", "no", "play", "makes", "Jack", "a", "dull", "boy"
+print(wd0,wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8, wd9 +".\n")
