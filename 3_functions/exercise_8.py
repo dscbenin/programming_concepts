6d861a3e1aefaea421f1f9e86b0e832bb85abbc2
Emmanuel <31184578+emthecoder@users.noreply.github.com>
2018-08-21 17:50:20 +0100

    Done.

--- 3_functions/exercise_8.py
+++ 3_functions/exercise_8.py
@@ -1,1 +1,6 @@
-#place your code here
+def circleArea(radius):
+    area=3.142*(radius**2)
+    return area
+
+radius=float(input("Enter Radius: "))
+print("The area of a circle with radius {}cm is {:.3f}cm~2".format(radius, circleArea(radius)))
